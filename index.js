bed19c4b3235eafbbbb65c9e1883201f426e9d9c
Steve <138328427+smajewski1980@users.noreply.github.com>
2025-04-04 19:28:23 -0400

    got the form data to get all the way to the endpoint, and send an error if incomplete

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const express = require("express");
 const app = express();
 const pg = require("pg");
+const { Pool } = pg;
+const pool = new Pool({
+  user: "postgres",
+  password: "postgres",
+  host: "localhost",
+  port: 5432,
+  database: "user_info",
+});
 
-app.use(express.json());
+pool
+  .connect()
+  .then(console.log("connected to the database: user_info"))
+  .catch((err) => {
+    console.error(err.message);
+  });
+
 app.use(express.static("public"));
+app.use(express.json());
+
+app.post("/user", (req, res) => {
+  const body = req.body;
+  for (const val in body) {
+    if (!body[val]) {
+      res.status(400).send();
+      return;
+    }
+  }
+  console.log(body);
+  res.status(201).send();
+});
 
 const PORT = process.env.PORT || 5500;
 app.listen(PORT, () => {
--- public/script.js
+++ public/script.js
@@ -4,7 +4,19 @@ const btnSubmit = document.querySelector("button");
 function handleSubmit(e) {
   e.preventDefault();
   const formData = new FormData(form);
-  console.log(formData);
+  const formVals = Object.fromEntries(formData.entries());
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formVals),
+  };
+  fetch("/user", options).then(async (res) => {
+    console.log("post request sent");
+  });
+  form.reset();
+  console.log(formVals);
 }
 
 btnSubmit.addEventListener("click", handleSubmit);
