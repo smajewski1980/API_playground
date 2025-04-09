ada3b07b20e1fce7a278165fb5f8b607dcab3f6d
Steve <138328427+smajewski1980@users.noreply.github.com>
2025-04-08 21:30:31 -0400

    got a few product images and css for basic card layout in products

--- public/prod_script.js
+++ public/prod_script.js
@@ -1,13 +1,32 @@
+const productsWrapper = document.querySelector(".products-wrapper");
 let productItems = [];
 
 const products = async () => {
-  await fetch("/product").then(async (res) => {
-    response = await res.json();
-    response.forEach((prod) => {
-      productItems.push(prod);
+  await fetch("/product")
+    .then(async (res) => {
+      response = await res.json();
+      response.forEach((prod) => {
+        productItems.push(prod);
+      });
+    })
+    .then(() => {
+      showProducts();
     });
-  });
 };
 products();
 
+function showProducts() {
+  productItems.forEach((item) => {
+    console.log(item);
+    productsWrapper.innerHTML += `
+    <div class="product-card">
+      <p>Product_id: ${item.product_id}</p>
+      <p>&nbsp;&nbsp;name: ${item.name}</p>
+      <p>&nbsp;&nbsp;price: ${item.price}</p>
+      <p>&nbsp;&nbsp;quantity: ${item.quantity}</p>
+      <img src=${item.img_url} alt=${item.name} />
+    </div>
+    `;
+  });
+}
 console.log(productItems);
