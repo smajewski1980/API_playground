2d6e7dc0b9379827092640a7d0e45463b4fb886e
Steve <138328427+smajewski1980@users.noreply.github.com>
2025-04-10 16:14:46 -0400

    got new product form working and cleared when submitted

--- public/add_product_script.js
+++ public/add_product_script.js
@@ -1,5 +1,6 @@
 const loginSpan = document.querySelector(".login-bug span");
 const btnAddProduct = document.querySelector("#btn-add-product");
+const addProductForm = document.getElementById("form-add-product");
 
 let loginStatus = () => {
   fetch("/login/status").then(async (res) => {
@@ -15,6 +16,19 @@ loginStatus();
 
 function handleAddProduct(e) {
   e.preventDefault();
+  const formData = new FormData(addProductForm);
+  const formVals = Object.fromEntries(formData.entries());
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formVals),
+  };
+  fetch("/product", options).then((res) => {
+    console.log("add product post request sent");
+    addProductForm.reset();
+  });
 }
 
 btnAddProduct.addEventListener("click", handleAddProduct);
